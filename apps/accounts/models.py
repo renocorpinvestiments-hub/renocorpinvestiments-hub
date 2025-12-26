99285f4934cd54cc07638158bb35d5368d4280e3
renocorpinvestiments-hub <renocorpinvestiments@gmail.com>
2025-12-26 07:00:02 +0300

    Update models.py

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import Group, Permission
 from django.db import models
 from django.contrib.auth.models import AbstractUser
