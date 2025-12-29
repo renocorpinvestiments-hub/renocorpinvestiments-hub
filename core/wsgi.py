d264572b475ad21c4f7c6396ef9f628fa28dfed4
renocorpinvestiments-hub <renocorpinvestiments@gmail.com>
2025-12-29 23:26:10 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -12,11 +12,10 @@ from django.contrib.auth import get_user_model
 def ensure_admin():
     User = get_user_model()
     username = "admin"
-    email = "admin@example.com"
     password = "Admin123!"
 
     if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
+        User.objects.create_superuser(username=username, password=password)
         print("✅ Admin account created")
     else:
         print("ℹ️ Admin account already exists")
