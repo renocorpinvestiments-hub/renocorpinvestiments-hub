f0d70b71b76475bf24be2b841b7d77b23ef2b286
renocorpinvestiments-hub <renocorpinvestiments@gmail.com>
2025-12-30 07:28:34 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -e
 
 # Install Python dependencies only
 pip install -r requirements.txt
-
+python manage.py repair_accounts
 # Skip migrations during build — the DB is unreachable at build time
