7464841a2897c3430ad5cdb27e534b54d23225e2
renocorpinvestiments-hub <renocorpinvestiments@gmail.com>
2025-12-30 07:45:22 +0300

    Update apps.py

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.accounts'
-
-    def ready(self):
-        # Import signals when the app is ready
-        from . import signals
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.accounts"
