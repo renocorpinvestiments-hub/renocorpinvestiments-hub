ba8bcd678b53e727b270d3876226eee38e67b253
renocorpinvestiments-hub <renocorpinvestiments@gmail.com>
2025-12-30 07:21:58 +0300

    Update apps.py

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -1,9 +1,21 @@
 from django.apps import AppConfig
+from django.db import transaction
 
 class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.accounts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.accounts"
 
     def ready(self):
-        # Import signals when the app is ready
-        from . import signals
+        from .models import User
+
+        try:
+            with transaction.atomic():
+                broken_users = User.objects.filter(
+                    account_number__isnull=True
+                ) | User.objects.filter(account_number="")
+
+                for user in broken_users:
+                    user.account_number = f"TEMP-{user.id}"
+                    user.save(update_fields=["account_number"])
+        except Exception:
+            pass
