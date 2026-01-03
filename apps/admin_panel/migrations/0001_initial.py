25a8f48630b74e4cd8a0742194eda9b0654894cc
renocorpinvestiments-hub <renocorpinvestiments@gmail.com>
2026-01-03 10:11:19 +0300

    Update 0001_initial.py

--- apps/admin_panel/migrations/0001_initial.py
+++ apps/admin_panel/migrations/0001_initial.py
@@ -1,1 +1,23 @@
+from django.db import migrations, models
+import django.utils.timezone
 
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('username', models.CharField(max_length=150, unique=True)),
+                ('email', models.EmailField(max_length=254, unique=True)),
+                ('password', models.CharField(max_length=128)),
+                ('is_active', models.BooleanField(default=True)),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now)),
+            ],
+        ),
+    ]
