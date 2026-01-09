47cf99dcb132685adc9c6f5aa168dff8eab99770
renocorpinvestiments-hub <renocorpinvestiments@gmail.com>
2026-01-09 11:32:11 +0300

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("signup/", views.signup_view, name="signup"),
     path("success/", views.signup_success_view, name="signup_success"),
+    path("logout/", views.logout_view, name="logout"),
 ]
