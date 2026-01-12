8291ef328ec9ed0fdd4ea4275f49587c7874dc58
renocorpinvestiments-hub <renocorpinvestiments@gmail.com>
2026-01-12 08:45:30 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -71,7 +71,18 @@ ADMIN_USERNAME = env('ADMIN_USERNAME', default='')
 ADMIN_PASSWORD = env('ADMIN_PASSWORD', default='')
 
 # -----------------------------------------------------------------------------
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
 # -----------------------------------------------------------------------------
 # DATABASE CONFIGURATION
 # -----------------------------------------------------------------------------
